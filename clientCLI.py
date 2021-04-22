06b798555be0787e754e3aac2ce2c86230014817
Mahavishnu <mahavishnureddya@gmail.com>
2021-04-22 04:29:34 -0400

    cliTool_initialCommit

---
+++ clientCLI.py
@@ -0,0 +1,3 @@
+# Idea is to raed .hex file then calculate checksum on the tool then we POST CHECKSUM
+# when we POST CHECKSUM we will get running checksum, if running checksum==the checksum
+# we caluculated in this tool then firmware is uploaded safely.
